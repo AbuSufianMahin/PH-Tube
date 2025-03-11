77de90ff4ba2f18ce13046bce0f742aa530dea0c
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-03-11 22:30:35 +0600

    added dynamic button from API

---
+++ script/index.js
@@ -0,0 +1,29 @@
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+
+        // converting response(promise) to json
+        .then((res) => res.json())
+
+        // displaying category arr from data using showCategories
+        .then((data) => showCatagories(data.categories))
+}
+
+function showCatagories(categoryArr){
+    // getting the parent element
+    const categoryContainer = document.getElementById("category-container");
+    
+    for (let catName of categoryArr){
+        // creating child element
+        const catDiv = document.createElement("div");
+
+        
+        // putting a buttton inside the individual category div (child element)
+        const categoryName = catName.category;
+        catDiv.innerHTML = `
+        <div class="btn btn-sm hover:bg-red-500 hover:text-white">${categoryName}</div> 
+        `
+        categoryContainer.appendChild(catDiv)
+    }
+
+}
+loadCategories();
